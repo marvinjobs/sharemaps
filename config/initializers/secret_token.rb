0f746b9cd132b9c48a450e0aa5712f429acf9a00
Marvin Jobs <marvin.jobs@gmail.com>
2013-03-21 19:26:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharemaps::Application.config.secret_token = '09df0ad2cb59189a65bbb6f7b600462bb568bc9e240caf74e01171f4d9a2038822a99c00690f1cef8669a726d2ceb2538560ad6b8bdc838c3b53b2ccb5569b66'
